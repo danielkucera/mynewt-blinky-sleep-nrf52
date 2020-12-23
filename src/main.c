4c17800d9b376c57658635d2161a1d1b0eb00f45
Daniel Kucera <daniel.kucera@gmail.com>
2020-12-23 13:53:19 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,116 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <assert.h>
+#include <string.h>
+
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+#ifdef ARCH_sim
+#include "mcu/mcu_sim.h"
+#endif
+
+static volatile int g_task1_loops;
+
+#define BLINK_INTERVAL 7  
+
+/* For LED toggling */
+int g_led_pin;
+
+static struct os_callout blinky_callout;
+
+static void
+timer_ev_cb(struct os_event *ev)
+{
+    assert(ev != NULL);
+
+    hal_gpio_toggle(g_led_pin);
+
+    os_callout_reset(&blinky_callout, BLINK_INTERVAL*OS_TICKS_PER_SEC);
+
+}
+
+static void
+init_timer(void)
+{
+    /*
+     * Initialize the callout for a timer event.
+     */
+    os_callout_init(&blinky_callout, os_eventq_dflt_get(),
+                    timer_ev_cb, NULL);
+
+    os_callout_reset(&blinky_callout, BLINK_INTERVAL*OS_TICKS_PER_SEC);
+
+}
+
+int pulse_cnt = 0;
+
+void pulse_handler(){
+    hal_gpio_toggle(g_led_pin);
+    pulse_cnt++;
+}
+
+/**
+ * main
+ *
+ * The main task for the project. This function initializes packages,
+ * and then blinks the BSP LED in a loop.
+ *
+ * @return int NOTE: this function should never return!
+ */
+int
+main(int argc, char **argv)
+{
+    int rc;
+
+    //NRF_CLOCK->TASKS_HFCLKSTOP = 1;
+
+#ifdef ARCH_sim
+    mcu_sim_parse_args(argc, argv);
+#endif
+
+    sysinit();
+
+    g_led_pin = LED_BLINK_PIN;
+    hal_gpio_init_out(g_led_pin, 1);
+
+    init_timer();
+
+    // deinit timer from bootloader
+    // LORA eats 1.6mA after powerup
+    // pin interrupt handler eats 8uA when MCU_GPIO_USE_PORT_EVENT=0
+    // SWD eats 3mA
+    // timer 0 eata 370uA
+    hal_timer_deinit(0);
+
+    hal_gpio_irq_init(17, pulse_handler, NULL, HAL_GPIO_TRIG_FALLING, HAL_GPIO_PULL_NONE);
+    hal_gpio_irq_enable(17);
+
+
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+
+    assert(0);
+
+    return rc;
+}
+
